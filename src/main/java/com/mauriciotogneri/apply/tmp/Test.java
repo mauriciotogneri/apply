215610f2208e1e210b5c61fcb988d0c5e16e7c7f
mtogneri <mtogneri@openwt.com>
2016-08-23 21:13:43 +0200

    Fixed tests

--- src/main/java/com/mauriciotogneri/apply/tmp/Test.java
+++ src/main/java/com/mauriciotogneri/apply/tmp/Test.java
@@ -1,25 +1,19 @@
 package com.mauriciotogneri.apply.tmp;
 
-import static com.mauriciotogneri.apply.experiment.Runtime.NumberOperations.equal;
-import static com.mauriciotogneri.apply.experiment.Runtime.NumberOperations.mul;
-import static com.mauriciotogneri.apply.experiment.Runtime.NumberOperations.sub;
+import static com.mauriciotogneri.apply.experiment.Runtime.AnyOperations.*;
+import static com.mauriciotogneri.apply.experiment.Runtime.NumberOperations.*;
 
 public class Test
 {
-    public static Number factorial(Number n)
-    {
-        if (equal(n, 0))
-        {
-            return 1;
-        }
-        else
-        {
-            return mul(n, factorial(sub(n, 1)));
-        }
-    }
-
-    public static void main(String[] args)
-    {
-        System.out.print(factorial(5));
-    }
+public static Number factorial(Number n)
+{
+if (equal(n, 0))
+{
+return 1;
+}
+else
+{
+return mul(n, factorial(sub(n, 1)));
+}
+}
 }
